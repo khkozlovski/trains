5638c8a410f9e6ddeb4cf9ac2e83c877846b3a1e
khkozlovski <khkozlovski@gmail.com>
2025-04-01 11:20:36 +0200

    New scrapped positions, script takes a look at all new books on the list, no author doesn't brake the url

--- trains.py
+++ trains.py
@@ -14,19 +14,21 @@ async def main():
             # FTYMI for position in range(100)
             #     if position not in read_books.txt
             #         book = await page.query_selector(f'#book-list article:nth-child({position})')
-            book = await page.query_selector('#book-list article:nth-child(9)')
-            result = dict()
-            title_el = await book.query_selector('h2  a')
-            result['title'] = await title_el.inner_text() if title_el else None
-            author_el = await book.query_selector('h3  a')
-            result['author'] = await author_el.inner_text() if author_el else None
-            print(result)
-            author_name = await author_el.inner_text()
-            author_last_name = author_name.split()[-1].lower()
-            title = await title_el.inner_text()
-            title_slug = title.replace(' ', '-').lower()
-            book_url = 'https://wolnelektury.pl/katalog/lektura/' + f'{author_last_name}' + '-' + f'{title_slug}' + '.html'
-            print(book_url)
+            # book = await page.query_selector('#book-list article:nth-child(9)')
+            for position in range(100):
+                book = await page.query_selector(new_book_titles[position])
+                result = dict()
+                title_el = await book.query_selector('h2  a')
+                result['title'] = await title_el.inner_text() if title_el else None
+                author_el = await book.query_selector('h3  a')
+                result['author'] = await author_el.inner_text() if author_el else None
+                print(result)
+                author_name = await author_el.inner_text() if author_el else None
+                author_last_name = author_name.split()[-1].lower() if author_name else None
+                title = await title_el.inner_text()
+                title_slug = title.replace(' ', '-').lower()
+                book_url = 'https://wolnelektury.pl/katalog/lektura/' + f'{author_last_name}' + '-' + f'{title_slug}' + '.html'
+                print(book_url)
 
             cover = await book.query_selector('figure a img')
             try:
@@ -77,41 +79,73 @@ async def main():
             await browser.close()
 
 
-def new_book_titles() -> [str]:
-    return ["#book-list article:nth-child(1)", "#book-list article:nth-child(2)", "#book-list article:nth-child(3)",
-            "#book-list article:nth-child(4)", "#book-list article:nth-child(5)", "#book-list article:nth-child(6)",
-            "#book-list article:nth-child(7)", "#book-list article:nth-child(8)", "#book-list article:nth-child(9)",
-            "#book-list article:nth-child(10)", "#book-list article:nth-child(11)", "#book-list article:nth-child(12)",
-            "#book-list article:nth-child(13)", "#book-list article:nth-child(14)", "#book-list article:nth-child(15)",
-            "#book-list article:nth-child(16)", "#book-list article:nth-child(17)", "#book-list article:nth-child(18)",
-            "#book-list article:nth-child(19)", "#book-list article:nth-child(20)", "#book-list article:nth-child(21)",
-            "#book-list article:nth-child(22)", "#book-list article:nth-child(23)", "#book-list article:nth-child(24)",
-            "#book-list article:nth-child(25)", "#book-list article:nth-child(26)", "#book-list article:nth-child(27)",
-            "#book-list article:nth-child(28)", "#book-list article:nth-child(29)", "#book-list article:nth-child(30)",
-            "#book-list article:nth-child(31)", "#book-list article:nth-child(32)", "#book-list article:nth-child(33)",
-            "#book-list article:nth-child(34)", "#book-list article:nth-child(35)", "#book-list article:nth-child(36)",
-            "#book-list article:nth-child(37)", "#book-list article:nth-child(38)", "#book-list article:nth-child(39)",
-            "#book-list article:nth-child(40)", "#book-list article:nth-child(41)", "#book-list article:nth-child(42)",
-            "#book-list article:nth-child(43)", "#book-list article:nth-child(44)", "#book-list article:nth-child(45)",
-            "#book-list article:nth-child(46)", "#book-list article:nth-child(47)", "#book-list article:nth-child(48)",
-            "#book-list article:nth-child(49)", "#book-list article:nth-child(50)", "#book-list article:nth-child(51)",
-            "#book-list article:nth-child(52)", "#book-list article:nth-child(53)", "#book-list article:nth-child(54)",
-            "#book-list article:nth-child(55)", "#book-list article:nth-child(56)", "#book-list article:nth-child(57)",
-            "#book-list article:nth-child(58)", "#book-list article:nth-child(59)", "#book-list article:nth-child(60)",
-            "#book-list article:nth-child(61)", "#book-list article:nth-child(62)", "#book-list article:nth-child(63)",
-            "#book-list article:nth-child(64)", "#book-list article:nth-child(65)", "#book-list article:nth-child(66)",
-            "#book-list article:nth-child(67)", "#book-list article:nth-child(68)", "#book-list article:nth-child(69)",
-            "#book-list article:nth-child(70)", "#book-list article:nth-child(71)", "#book-list article:nth-child(72)",
-            "#book-list article:nth-child(73)", "#book-list article:nth-child(74)", "#book-list article:nth-child(75)",
-            "#book-list article:nth-child(76)", "#book-list article:nth-child(77)", "#book-list article:nth-child(78)",
-            "#book-list article:nth-child(79)", "#book-list article:nth-child(80)", "#book-list article:nth-child(81)",
-            "#book-list article:nth-child(82)", "#book-list article:nth-child(83)", "#book-list article:nth-child(84)",
-            "#book-list article:nth-child(85)", "#book-list article:nth-child(86)", "#book-list article:nth-child(87)",
-            "#book-list article:nth-child(88)", "#book-list article:nth-child(89)", "#book-list article:nth-child(90)",
-            "#book-list article:nth-child(91)", "#book-list article:nth-child(92)", "#book-list article:nth-child(93)",
-            "#book-list article:nth-child(94)", "#book-list article:nth-child(95)", "#book-list article:nth-child(96)",
-            "#book-list article:nth-child(97)", "#book-list article:nth-child(98)", "#book-list article:nth-child(99)",
-            "#book-list article:nth-child(100)"]
+new_book_titles = ["#book-list article:nth-child(1)", "#book-list article:nth-child(2)",
+                   "#book-list article:nth-child(3)",
+                   "#book-list article:nth-child(4)", "#book-list article:nth-child(5)",
+                   "#book-list article:nth-child(6)",
+                   "#book-list article:nth-child(7)", "#book-list article:nth-child(8)",
+                   "#book-list article:nth-child(9)",
+                   "#book-list article:nth-child(10)", "#book-list article:nth-child(11)",
+                   "#book-list article:nth-child(12)",
+                   "#book-list article:nth-child(13)", "#book-list article:nth-child(14)",
+                   "#book-list article:nth-child(15)",
+                   "#book-list article:nth-child(16)", "#book-list article:nth-child(17)",
+                   "#book-list article:nth-child(18)",
+                   "#book-list article:nth-child(19)", "#book-list article:nth-child(20)",
+                   "#book-list article:nth-child(21)",
+                   "#book-list article:nth-child(22)", "#book-list article:nth-child(23)",
+                   "#book-list article:nth-child(24)",
+                   "#book-list article:nth-child(25)", "#book-list article:nth-child(26)",
+                   "#book-list article:nth-child(27)",
+                   "#book-list article:nth-child(28)", "#book-list article:nth-child(29)",
+                   "#book-list article:nth-child(30)",
+                   "#book-list article:nth-child(31)", "#book-list article:nth-child(32)",
+                   "#book-list article:nth-child(33)",
+                   "#book-list article:nth-child(34)", "#book-list article:nth-child(35)",
+                   "#book-list article:nth-child(36)",
+                   "#book-list article:nth-child(37)", "#book-list article:nth-child(38)",
+                   "#book-list article:nth-child(39)",
+                   "#book-list article:nth-child(40)", "#book-list article:nth-child(41)",
+                   "#book-list article:nth-child(42)",
+                   "#book-list article:nth-child(43)", "#book-list article:nth-child(44)",
+                   "#book-list article:nth-child(45)",
+                   "#book-list article:nth-child(46)", "#book-list article:nth-child(47)",
+                   "#book-list article:nth-child(48)",
+                   "#book-list article:nth-child(49)", "#book-list article:nth-child(50)",
+                   "#book-list article:nth-child(51)",
+                   "#book-list article:nth-child(52)", "#book-list article:nth-child(53)",
+                   "#book-list article:nth-child(54)",
+                   "#book-list article:nth-child(55)", "#book-list article:nth-child(56)",
+                   "#book-list article:nth-child(57)",
+                   "#book-list article:nth-child(58)", "#book-list article:nth-child(59)",
+                   "#book-list article:nth-child(60)",
+                   "#book-list article:nth-child(61)", "#book-list article:nth-child(62)",
+                   "#book-list article:nth-child(63)",
+                   "#book-list article:nth-child(64)", "#book-list article:nth-child(65)",
+                   "#book-list article:nth-child(66)",
+                   "#book-list article:nth-child(67)", "#book-list article:nth-child(68)",
+                   "#book-list article:nth-child(69)",
+                   "#book-list article:nth-child(70)", "#book-list article:nth-child(71)",
+                   "#book-list article:nth-child(72)",
+                   "#book-list article:nth-child(73)", "#book-list article:nth-child(74)",
+                   "#book-list article:nth-child(75)",
+                   "#book-list article:nth-child(76)", "#book-list article:nth-child(77)",
+                   "#book-list article:nth-child(78)",
+                   "#book-list article:nth-child(79)", "#book-list article:nth-child(80)",
+                   "#book-list article:nth-child(81)",
+                   "#book-list article:nth-child(82)", "#book-list article:nth-child(83)",
+                   "#book-list article:nth-child(84)",
+                   "#book-list article:nth-child(85)", "#book-list article:nth-child(86)",
+                   "#book-list article:nth-child(87)",
+                   "#book-list article:nth-child(88)", "#book-list article:nth-child(89)",
+                   "#book-list article:nth-child(90)",
+                   "#book-list article:nth-child(91)", "#book-list article:nth-child(92)",
+                   "#book-list article:nth-child(93)",
+                   "#book-list article:nth-child(94)", "#book-list article:nth-child(95)",
+                   "#book-list article:nth-child(96)",
+                   "#book-list article:nth-child(97)", "#book-list article:nth-child(98)",
+                   "#book-list article:nth-child(99)",
+                   "#book-list article:nth-child(100)"]
 
 
 def train_phrases() -> list[str]:
@@ -127,9 +161,11 @@ def train_phrases_iterator() -> list[str]:
     for train_phrase in train_phrases():
         yield train_phrase
 
-def new_book_titles_iterator() -> list[str]:
-    for publication in new_book_titles():
-        yield publication
+
+# def new_book_titles_iterator() -> list[str]:
+#     for publication in new_book_titles():
+#         yield publication
+
 
 async def refuse_help(page):
     result = dict()
